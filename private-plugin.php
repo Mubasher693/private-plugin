83f91457a77e09b6fbbae0637903d64c50eccc52
Mubasher <mubasheriqbal693@gmail.com>
2023-03-10 19:42:14 +0500

    adding plugin annotation

--- private-plugin.php
+++ private-plugin.php
@@ -1,8 +1,13 @@
 <?php
 
 /**
- * Plugin Name: Private Plugin
- */
+ * Plugin Name: private-plugin
+ * Plugin URI: https://www.your-site.com/
+ * Description: Demonstration for, How we can install private plugin in bedrock..
+ * Version: 0.1
+ * Author: Mubasher iqbal
+ * Author URI: https://github.com/Mubasher693
+ **/
 
 if (!is_admin()) {
     echo '<p>Hi from my private plugin<p/>';
